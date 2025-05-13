c16e972e15634fcda35b8ca75b743428f87eae81
tilio-p <160761302+tilio-p@users.noreply.github.com>
2025-05-13 11:24:50 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const circle = document.querySelector(".circle");
   const titre1 = document.getElementById("title1");
   const titre2 = document.getElementById("title2");
+  const text = document.getElementById("text");
   let step = 0;
 
   circle.addEventListener("animationiteration", () => {
@@ -12,6 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
         circle.style.background = "#fd0"; // Jaune
         titre1.textContent = "Envie de";
         titre2.textContent = "Culture ?";
+        text.textContent = "Lu 17.03 - Di 25.05";
         break;
       case 1: // Aller 1 terminé → Retour 1
         circle.style.background = "#25FFA4"; // Vert
@@ -19,6 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
       case 2: // Retour 1 terminé → Aller 2
         titre1.textContent = "Lust auf";
         titre2.textContent = "Kultur ?";
+        text.textContent = "Mo 17.03 - Do 25.05";
         circle.style.background = "#00B2FF"; // Bleu
         break;
       case 3: // Aller 2 terminé → Retour 2
